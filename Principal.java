9f341607c76a6815ff55bdb1f24d6d29f5c02cb7
Alejandro Ramirez <aaleex8842@gmail.com>
2024-03-08 11:09:28 +0100

    ModifLoc-Rem

--- Principal.java
+++ Principal.java
@@ -5,7 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		System.out.println(" Hola a tod@s ");
-		System.out.println(" Cambios desde remoto ")
+		System.out.println(" Cambios desde remoto ");
+		System.out.println("Cambios desde local a remoto");
 
 	}
 
